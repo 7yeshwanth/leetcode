164ae54f20373c6e09a16c739a889a9a5355d42e
Yeshwanth <107457640+7yeshwanth@users.noreply.github.com>
2025-01-02 00:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (11.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    map<int, int> m;
+    map<int, int> m = {{0,0}, {1,1}, {2, 1}};
     int fib(int n) {
         if (m.find(n) != m.end()) {
             return m[n];
-        }
-        if (n == 0) {
-            return 0;
-        }
-        if (n <= 2) {
-            return 1;
         }
         m[n-1] = fib(n-1);
         m[n-2] = fib(n-2);
