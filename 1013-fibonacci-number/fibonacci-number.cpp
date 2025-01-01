091e0d7de302e05f62afbfe74ee7f952fa560086
Yeshwanth <107457640+7yeshwanth@users.noreply.github.com>
2025-01-01 23:59:09 +0530

    Time: 3 ms (64.18%) | Memory: 8.1 MB (11.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -11,6 +11,8 @@ public:
         if (n <= 2) {
             return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        m[n-1] = fib(n-1);
+        m[n-2] = fib(n-2);
+        return m[n-1] + m[n-2];
     }
 };
