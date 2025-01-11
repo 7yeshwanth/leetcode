ea7e05243ca91f1c92dca7f4878b87165947a5b2
Yeshwanth <107457640+7yeshwanth@users.noreply.github.com>
2025-01-12 01:23:03 +0530

    Time: 31 ms (68.28%) | Memory: 14.7 MB (74.41%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,54 +1,30 @@
-class Solution
-{
-  bool u(string s, string p, int ns, int np, vector<vector<int>> &dp)
-  {
-    if (ns < 0 && np < 0)
-    {
-      return 1;
-    }
-    if (ns < 0)
-    {
-      if (np == 0 && p[0] == '*')
-        return 1;
-      return 0;
-    }
-    if (np < 0)
-    {
-      return 0;
-    }
-    if (dp[ns][np] == -1)
-    {
-      if (s[ns] == p[np] || p[np] == '?')
-      {
-        return dp[ns][np] = u(s, p, ns - 1, np - 1, dp);
-      }
-      if (p[np] == '*')
-      {
-        return dp[ns][np] = u(s, p, ns, np - 1, dp) ||
-                            u(s, p, ns - 1, np, dp);
-      }
-    }
-    else
-      return dp[ns][np];
-    return 0;
-  };
-
+class Solution {
 public:
-  bool isMatch(string s, string p)
-  {
-    int ns = s.size(), np = p.size();
-    if (ns == np && ns == 0)
-      return 1;
-    string pp;
-    pp += p[0];
-    for (int i = 1; i < np; i++)
-    {
-      if (p[i] == '*' && pp.back() == p[i])
-        continue;
-      pp += p[i];
+    bool isMatch(string s, string p) {
+        int ns = s.size(), np = p.size();
+        vector<vector<bool>> dp(ns + 1, vector<bool>(np + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= ns; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= np; j++) {
+            bool fl = 1;
+            for (int jj = 1; jj <= j; jj++) {
+                if (p[jj - 1] != '*')
+                    fl = 0;
+            }
+            dp[0][j] = fl;
+        }
+        for (int i = 1; i < ns + 1; i++) {
+            for (int j = 1; j < np + 1; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return dp[ns][np];
     }
-    np = pp.size();
-    vector<vector<int>> dp(ns, vector<int>(np, -1));
-    return u(s, pp, ns - 1, np - 1, dp);
-  }
 };
