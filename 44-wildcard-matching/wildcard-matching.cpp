c110cb1e5c19b3b7a90034ac0d6a0d89f568def4
Yeshwanth <107457640+7yeshwanth@users.noreply.github.com>
2025-01-11 22:57:53 +0530

    Time: 944 ms (6.02%) | Memory: 636.6 MB (5.16%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,41 +1,54 @@
-class Solution {
-private:
-    bool solveMem(string& s, string& p, int i, int j, vector<vector<int>>& dp) {
-        // base case
-        if (i == 0 && j == 0)
-            return true;
-
-        if (i > 0 && j == 0)
-            return false;
-
-        if (i == 0 && j > 0) {
-            for (int k = 1; k <= j; k++) {
-                if (p[k - 1] != '*')
-                    return false;
-            }
-            return true;
-        }
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        // match
-        if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
-            return dp[i][j] = solveMem(s, p, i - 1, j - 1, dp);
-        else if (p[j - 1] == '*')
-            return dp[i][j] = (solveMem(s, p, i - 1, j, dp) ||
-                               solveMem(s, p, i, j - 1, dp));
-        else
-            return false;
+class Solution
+{
+  bool u(string s, string p, int ns, int np, vector<vector<int>> &dp)
+  {
+    if (ns < 0 && np < 0)
+    {
+      return 1;
     }
+    if (ns < 0)
+    {
+      if (np == 0 && p[0] == '*')
+        return 1;
+      return 0;
+    }
+    if (np < 0)
+    {
+      return 0;
+    }
+    if (dp[ns][np] == -1)
+    {
+      if (s[ns] == p[np] || p[np] == '?')
+      {
+        return dp[ns][np] = u(s, p, ns - 1, np - 1, dp);
+      }
+      if (p[np] == '*')
+      {
+        return dp[ns][np] = u(s, p, ns, np - 1, dp) ||
+                            u(s, p, ns - 1, np, dp);
+      }
+    }
+    else
+      return dp[ns][np];
+    return 0;
+  };
 
 public:
-    bool isMatch(string s, string p) {
-
-        int n = s.length();
-        int m = p.length();
-
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solveMem(s, p, n, m, dp);
-    };
+  bool isMatch(string s, string p)
+  {
+    int ns = s.size(), np = p.size();
+    if (ns == np && ns == 0)
+      return 1;
+    string pp;
+    pp += p[0];
+    for (int i = 1; i < np; i++)
+    {
+      if (p[i] == '*' && pp.back() == p[i])
+        continue;
+      pp += p[i];
+    }
+    np = pp.size();
+    vector<vector<int>> dp(ns, vector<int>(np, -1));
+    return u(s, pp, ns - 1, np - 1, dp);
+  }
 };
